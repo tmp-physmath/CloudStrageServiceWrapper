a88a82344d4955c782242ae379996c046ce8175b
NAMIYAMA_KENSUKE <namiken2525@yahoo.co.jp>
2014-11-14 17:43:10 +0900

    dorpboxファイル追加機能を追加

---
+++ src/system/ErrorConst.java
@@ -0,0 +1,11 @@
+package system;
+
+public class ErrorConst {
+	public static final int SUCCESS_PROCESS = 1;
+
+	public static final int CANNOT_AUTH = 1 << 1; 
+	
+	public static final int EXCEPTION_IOEXCEPTION = 1 << 2;
+
+	public static final int EXCEPTION_EXCEPTION = 1 << 3;
+}
