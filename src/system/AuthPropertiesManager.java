66c2ab06674dccd72d67293c6afd701475d1900a
tmp-physmath <satoru.physmath@gmail.com>
2014-11-21 18:53:29 +0900

    AuthPropertiesManager実装

---
+++ src/system/AuthPropertiesManager.java
@@ -0,0 +1,107 @@
+package system;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+
+public final class AuthPropertiesManager{
+	File authinfo_dir_ = null;
+	static private AuthPropertiesManager instance = new AuthPropertiesManager();
+	static public AuthPropertiesManager getInstance(){
+		return instance;
+	}
+	
+	boolean existAuthInfoDir(){
+		return authinfo_dir_ != null && authinfo_dir_.exists();
+	}
+	
+	private AuthPropertiesManager(){
+		File file = null;
+		try{
+			file  = new File(this.get_currentpath() + "/authinfo");
+			if(!file.isDirectory()){
+				System.out.println(file.getAbsolutePath() + "ディレクトリが存在しないので作成します");
+				file.mkdir();
+			}else{
+				System.out.println(file.getAbsolutePath() + "ディレクトリが存在します");
+			}
+		}catch(Exception e){
+			System.out.println("error!" + file.getAbsolutePath() + "にアクセスできません\n" + e.toString());
+		}
+		authinfo_dir_ = file;
+	}
+	
+
+	public Map<String,String> get(String user, String service){
+		try {
+			File authinfo = new File(authinfo_dir_.getAbsolutePath() + "/" + user + "." + service);
+			if(authinfo.canRead()) {
+				Properties prop = new Properties();
+				try (InputStream authinfo_fio = new FileInputStream(authinfo) ){
+					prop.load(authinfo_fio);
+				}
+				HashMap<String, String> hashMap = new HashMap<String, String>();
+				for (Entry<Object, Object> e : prop.entrySet()) {
+					hashMap.put(e.getKey().toString(), e.getValue().toString());
+				}
+				return hashMap;
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			return null;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return null;
+		} catch (NullPointerException e){
+			e.printStackTrace();
+			return null;
+		} 
+		return null;
+	}
+	
+	public boolean set(String user, String service, Map<String,String> data){
+		try {
+			File authinfo = new File(authinfo_dir_.getAbsolutePath() + "/" + user + "." + service);
+			if(!authinfo.exists()){
+				authinfo.createNewFile();
+			}
+			if(authinfo.canWrite()){
+				Properties prop = new Properties();
+				for( Map.Entry<String,String> e  : data.entrySet()){
+					prop.setProperty(e.getKey(), e.getValue());
+				}
+				try(FileOutputStream authinfo_fos= new FileOutputStream(authinfo)){
+					prop.store(authinfo_fos, "propertie : " + user + "." + service);
+				}
+				return true;
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return false;
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return false;
+		} catch(NullPointerException e){
+			e.printStackTrace();
+			return false;
+		}
+		System.out.println("error! : in AuthPropertiesManager.set()");
+		return false;
+	}
+	private String get_currentpath(){
+
+		String dirPath = System.getProperty("user.dir");
+		return dirPath;
+	}
+
+}
--- src/system/UserInterface.java
+++ src/system/UserInterface.java
@@ -1,12 +1,8 @@
 
 package system;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import storage.virtualStorage.VirtualStorage;
-import storage.virtualStorage.VirtualStorageManager;
+import java.util.HashMap;
+import java.util.Map;
 /*
-import storage.virtualStorage.command.DoNothing;
 import storage.virtualStorage.command.IVirtualStorageCommand;
 */
 public class UserInterface {
@@ -16,13 +12,23 @@ public class UserInterface {
 	 */
 	public static void main(String[] args) {
 		
-		ArrayList<String> argList = (ArrayList<String>) Arrays.asList(args);
+		System.out.println("does the auth directory exist : " + AuthPropertiesManager.getInstance().existAuthInfoDir());
+		/*
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("test_key2", "test_value2");
+		map.put("test_key3", "test_value3");
+		boolean flag = AuthPropertiesManager.getInstance().set("testuser", "testservice",  map);
+		System.out.println("does set succes : " + flag);
+		*/
+		Map<String,String> readmap = AuthPropertiesManager.getInstance().get("testuser", "testservice");
+		if(readmap == null){
+			System.out.println("プロパティファイルを読み込めませんでした");
+			return;
+		}
+		for(Map.Entry<String, String> entry : readmap.entrySet() ){
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
 		
-		System.out.println("running test\n");
-		VirtualStorage vstorage = VirtualStorageFactory.getInstance().create(null);
-		VirtualStorageManager.getInstance().setVirtualStorage(vstorage);
-		String ret = VirtualStorageManager.getInstance().operate(argList);
-		System.out.println(ret);
 	}
 	/*private static int operateVirtualStorage(String[] args){
 		IVirtualStorageCommand command = new DoNothing();
